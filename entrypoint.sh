8bce398107e3cd1b232c28a275991515cb96e270
nandusekarv10 <31997422+nandusekarv10@users.noreply.github.com>
2021-02-16 13:12:41 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,12 @@ fi
 echo "Hello echoing INPUT_EXCLUDE_RULES"
 echo ${INPUT_EXCLUDERULES}
 
+echo "Hello printing INPUT_FAILIF
+echo ${INPUT_FAILIF}
+
+echo "printing INPUT_TOKEN"
+echo ${INPUT_TOKEN}
+
 ls -la /
 
 cfn_nag_scan --input-path ${INPUT_TEMPLATEPATH} --blacklist-path=${INPUT_EXCLUDERULES} --output-format=json | lintly --api-key $INPUT_TOKEN \
