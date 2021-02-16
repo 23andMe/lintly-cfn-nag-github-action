a91621de2ac652738c37489f3b7d4d6e311dd544
nandusekarv10 <31997422+nandusekarv10@users.noreply.github.com>
2021-02-16 13:02:20 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,6 @@ echo ${INPUT_EXCLUDERULES}
 
 ls -la /
 
-#cfn_nag_scan --input-path ${INPUT_TEMPLATEPATH} --blacklist-path=${INPUT_EXCLUDERULES} --output-format=json | lintly --api-key $INPUT_TOKEN \
-#        --fail-on $INPUT_FAILIF \
-#       --log --no-post-status --format=cfn-nag
-
-
 cfn_nag_scan --input-path ${INPUT_TEMPLATEPATH} --blacklist-path=${INPUT_EXCLUDERULES} --output-format=json | lintly --api-key $INPUT_TOKEN \
         --fail-on $INPUT_FAILIF \
-       --log --no-post-status --format=cfn-lint
+       --log --no-post-status --format=cfn-nag
